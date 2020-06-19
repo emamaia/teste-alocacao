e5b0325fed8a0659a1d3e9dec1a3f72fcf6856c1
E. Maia Moreira <e.maia.moreira@avanade.com>
2020-06-18 22:32:43 -0300

    Atualiza carousel cards

--- index.js
+++ index.js
@@ -4,62 +4,64 @@ const menu = document.getElementById('menu');
 const url = "https://sky-frontend.herokuapp.com/movies";
 
 async function getApi(url) {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const json = await response.json();
-            console.log(json);
-            getCarousel(json[0].items);
-            getCards(json[2].movies)
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const json = await response.json();
+      console.log(json);
+      getCarousel(json[0].items);
+      getCards(json[2].movies)
 
-        } else {
-            const err = `${response.status}: ${response.statusText}`;
-        }
+    } else {
+      const err = `${response.status}: ${response.statusText}`;
     }
+  }
 
-    catch (error) {
-        criaErro('Falha na requisição da API');
-    }
+  catch (error) {
+    criaErro('Falha na requisição da API');
+  }
 }
 
 criaErro = (erro) => {
-    document.body.innerHTML = `<h1 class='text-danger'>${erro}</h1>`;
+  document.body.innerHTML = `<h1 class='text-danger'>${erro}</h1>`;
 }
 
 
 function getCarousel(items) {
-    console.log(items);
-    let itemCarousel = document.createElement('div')
-    carousel.appendChild(itemCarousel)
-    itemCarousel.innerHTML = `
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  console.log(items);
+  let itemCarousel = document.createElement('div')
+  carousel.appendChild(itemCarousel)
+  itemCarousel.innerHTML = `
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
-    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
-  <div class="carousel-inner mt-4 image-carousel">
+  <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="${items[0].images[0].url}" class="d-block w-100" alt="Imagem filme">     
+      <img src="${items[0].images[0].url}" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="${items[1].images[0].url}" class="d-block w-100" alt="Imagem filme">     
+      <img src="${items[1].images[0].url}" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="${items[2].images[0].url}" class="d-block w-100" alt="Imagem filme">      
+      <img src="${items[2].images[0].url}" class="d-block w-100" alt="...">
     </div>
-    <div class="carousel-item">
-      <img src="${items[3].images[0].url}" class="d-block w-100" alt="Imagem filme">     
+    <div class="carousel-item ">
+    <img src="${items[3].images[0].url}" class="d-block w-100" alt="...">
     </div>
-    <div class="carousel-item">
-    <img src="${items[4].images[0].url}" class="d-block w-100" alt="Imagem filme">    
+    <div class="carousel-item ">
+      <img src="${items[4].images[0].url}" class="d-block w-100" alt="...">
+    </div>
+
+
   </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
@@ -69,40 +71,77 @@ function getCarousel(items) {
 
 
 function getCards(card) {
-    console.log(card);
-    let categories = new Map();
-    for (let i = 0; i < card.length; i++) {
-        let splits = card[i].categories.split(', ');
-        console.log(splits)
-        splits.forEach(element => {
-            console.log(element)
-            let cardsCategories = categories.get(element);
-            if (cardsCategories === undefined) {
-                cardsCategories = [card[i]];
-                categories.set(element, cardsCategories);
-            } else {
-                cardsCategories.push(card[i])
-            }
-        });
+  console.log(card);
+  let categories = new Map();
+  for (let i = 0; i < card.length; i++) {
+    let splits = card[i].categories.split(', ');
+    console.log(splits)
+    splits.forEach(element => {
+      console.log(element)
+      let cardsCategories = categories.get(element);
+      if (cardsCategories === undefined) {
+        cardsCategories = [card[i]];
+        categories.set(element, cardsCategories);
+      } else {
+        cardsCategories.push(card[i])
+      }
+    });
+  }
+
+  categories.forEach(function (cards, categoria) {
+    let divCarousel = document.createElement('div')
+    menu.appendChild(divCarousel);   
+    divCarousel.classList.add('swiper-container', 'div-categoria')
+
+    let titleCategoria = document.createElement('h5')
+    titleCategoria.innerHTML += categoria
+    divCarousel.appendChild(titleCategoria)
+   
+
+    let divCards = document.createElement('div')
+    divCarousel.appendChild(divCards)
+    divCards.classList.add('swiper-wrapper')
+
+    for (let i = 0; i < cards.length; i++) {
+      divCards.innerHTML = `
+          ${divCards.innerHTML} 
+          <div class="swiper-slide">
+            <img class="card" src="${cards[i].images[0].url}" alt="Card do filme">
+          </div>`;
     }
 
-    categories.forEach(function (cards, categoria) {
-        let divCategoria = document.createElement('div')
-        menu.appendChild(divCategoria);
-        let titleCategoria = document.createElement('h5')
-        menu.appendChild(titleCategoria)
-        divCategoria.classList.add('div-categoria')
-        titleCategoria.innerHTML += categoria
-        for (let i = 0; i < cards.length; i++) {
-            let menuCard = document.createElement('div');
-            divCategoria.appendChild(menuCard);
-            menuCard.innerHTML += `<img class="card" src="${cards[i].images[0].url}" alt="Card do filme">`
-        }
+    divCarousel.innerHTML += `
+    <div class="btn swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+    `
+  }, categories)  
 
-    }, categories)
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: "horizontal",
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 6,
+  slidesPerGroup: 6,
+  height: 200,
+  slidesOffsetBefore: 0,
 
-   
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  })
 }
-
 
 getApi(url)
